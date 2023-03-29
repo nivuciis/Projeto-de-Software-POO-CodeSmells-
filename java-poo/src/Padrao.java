64514e46c911ff15edc724de33029db1c7c51861
Jeyson <jeyson-santos@hotmail.com>
2023-03-29 14:17:25 -0300

    Finalizando programa.

--- java-poo/src/Padrao.java
+++ java-poo/src/Padrao.java
@@ -3,6 +3,7 @@ abstract class Padrao{
     public abstract void setNome();
     public abstract void setCPF();
     public abstract void setEmail();
-
-
+    public abstract String getNome();
+    public abstract String getCpf();
+    public abstract String getEmail();
 }
